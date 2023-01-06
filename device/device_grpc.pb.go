df0376a682f960565e64fa4b8733ffa4d2159874
朱厚九 <gongcheng.zhu@tuya.com>
2023-01-06 15:45:30 +0800

    fix

--- device/device_grpc.pb.go
+++ device/device_grpc.pb.go
@@ -24,11 +24,13 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type RpcDeviceClient interface {
 	// 设备连接云服务
-	ConnectIotCloud(ctx context.Context, in *ConnectIotPlatformRequest, opts ...grpc.CallOption) (*common.CommonResponse, error)
+	ConnectIotPlatform(ctx context.Context, in *ConnectIotPlatformRequest, opts ...grpc.CallOption) (*common.CommonResponse, error)
 	// 设备断开连接云服务
 	DisconnectIotPlatform(ctx context.Context, in *DisconnectIotPlatformRequest, opts ...grpc.CallOption) (*common.CommonResponse, error)
 	// 获取所有设备
 	QueryDeviceList(ctx context.Context, in *QueryDeviceListRequest, opts ...grpc.CallOption) (*QueryDeviceListResponse, error)
+	// 获取设备
+	QueryDeviceById(ctx context.Context, in *QueryDeviceByIdRequest, opts ...grpc.CallOption) (*QueryDeviceByIdRequestResponse, error)
 	// 创建设备
 	CreateDevice(ctx context.Context, in *CreateDeviceRequest, opts ...grpc.CallOption) (*common.CommonResponse, error)
 	// 创建设备并且建立连接
@@ -45,9 +47,9 @@ func NewRpcDeviceClient(cc grpc.ClientConnInterface) RpcDeviceClient {
 	return &rpcDeviceClient{cc}
 }
 
-func (c *rpcDeviceClient) ConnectIotCloud(ctx context.Context, in *ConnectIotPlatformRequest, opts ...grpc.CallOption) (*common.CommonResponse, error) {
+func (c *rpcDeviceClient) ConnectIotPlatform(ctx context.Context, in *ConnectIotPlatformRequest, opts ...grpc.CallOption) (*common.CommonResponse, error) {
 	out := new(common.CommonResponse)
-	err := c.cc.Invoke(ctx, "/device.RpcDevice/ConnectIotCloud", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/device.RpcDevice/ConnectIotPlatform", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -66,6 +68,15 @@ func (c *rpcDeviceClient) DisconnectIotPlatform(ctx context.Context, in *Disconn
 func (c *rpcDeviceClient) QueryDeviceList(ctx context.Context, in *QueryDeviceListRequest, opts ...grpc.CallOption) (*QueryDeviceListResponse, error) {
 	out := new(QueryDeviceListResponse)
 	err := c.cc.Invoke(ctx, "/device.RpcDevice/QueryDeviceList", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *rpcDeviceClient) QueryDeviceById(ctx context.Context, in *QueryDeviceByIdRequest, opts ...grpc.CallOption) (*QueryDeviceByIdRequestResponse, error) {
+	out := new(QueryDeviceByIdRequestResponse)
+	err := c.cc.Invoke(ctx, "/device.RpcDevice/QueryDeviceById", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -104,11 +115,13 @@ func (c *rpcDeviceClient) DeleteDevice(ctx context.Context, in *DeleteDeviceRequ
 // for forward compatibility
 type RpcDeviceServer interface {
 	// 设备连接云服务
-	ConnectIotCloud(context.Context, *ConnectIotPlatformRequest) (*common.CommonResponse, error)
+	ConnectIotPlatform(context.Context, *ConnectIotPlatformRequest) (*common.CommonResponse, error)
 	// 设备断开连接云服务
 	DisconnectIotPlatform(context.Context, *DisconnectIotPlatformRequest) (*common.CommonResponse, error)
 	// 获取所有设备
 	QueryDeviceList(context.Context, *QueryDeviceListRequest) (*QueryDeviceListResponse, error)
+	// 获取设备
+	QueryDeviceById(context.Context, *QueryDeviceByIdRequest) (*QueryDeviceByIdRequestResponse, error)
 	// 创建设备
 	CreateDevice(context.Context, *CreateDeviceRequest) (*common.CommonResponse, error)
 	// 创建设备并且建立连接
@@ -122,14 +135,17 @@ type RpcDeviceServer interface {
 type UnimplementedRpcDeviceServer struct {
 }
 
-func (UnimplementedRpcDeviceServer) ConnectIotCloud(context.Context, *ConnectIotPlatformRequest) (*common.CommonResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ConnectIotCloud not implemented")
+func (UnimplementedRpcDeviceServer) ConnectIotPlatform(context.Context, *ConnectIotPlatformRequest) (*common.CommonResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ConnectIotPlatform not implemented")
 }
 func (UnimplementedRpcDeviceServer) DisconnectIotPlatform(context.Context, *DisconnectIotPlatformRequest) (*common.CommonResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DisconnectIotPlatform not implemented")
 }
 func (UnimplementedRpcDeviceServer) QueryDeviceList(context.Context, *QueryDeviceListRequest) (*QueryDeviceListResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method QueryDeviceList not implemented")
+}
+func (UnimplementedRpcDeviceServer) QueryDeviceById(context.Context, *QueryDeviceByIdRequest) (*QueryDeviceByIdRequestResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method QueryDeviceById not implemented")
 }
 func (UnimplementedRpcDeviceServer) CreateDevice(context.Context, *CreateDeviceRequest) (*common.CommonResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateDevice not implemented")
@@ -153,20 +169,20 @@ func RegisterRpcDeviceServer(s grpc.ServiceRegistrar, srv RpcDeviceServer) {
 	s.RegisterService(&RpcDevice_ServiceDesc, srv)
 }
 
-func _RpcDevice_ConnectIotCloud_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _RpcDevice_ConnectIotPlatform_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(ConnectIotPlatformRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(RpcDeviceServer).ConnectIotCloud(ctx, in)
+		return srv.(RpcDeviceServer).ConnectIotPlatform(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/device.RpcDevice/ConnectIotCloud",
+		FullMethod: "/device.RpcDevice/ConnectIotPlatform",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(RpcDeviceServer).ConnectIotCloud(ctx, req.(*ConnectIotPlatformRequest))
+		return srv.(RpcDeviceServer).ConnectIotPlatform(ctx, req.(*ConnectIotPlatformRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -203,6 +219,24 @@ func _RpcDevice_QueryDeviceList_Handler(srv interface{}, ctx context.Context, de
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(RpcDeviceServer).QueryDeviceList(ctx, req.(*QueryDeviceListRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _RpcDevice_QueryDeviceById_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryDeviceByIdRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RpcDeviceServer).QueryDeviceById(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/device.RpcDevice/QueryDeviceById",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RpcDeviceServer).QueryDeviceById(ctx, req.(*QueryDeviceByIdRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -269,8 +303,8 @@ var RpcDevice_ServiceDesc = grpc.ServiceDesc{
 	HandlerType: (*RpcDeviceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "ConnectIotCloud",
-			Handler:    _RpcDevice_ConnectIotCloud_Handler,
+			MethodName: "ConnectIotPlatform",
+			Handler:    _RpcDevice_ConnectIotPlatform_Handler,
 		},
 		{
 			MethodName: "DisconnectIotPlatform",
@@ -279,6 +313,10 @@ var RpcDevice_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "QueryDeviceList",
 			Handler:    _RpcDevice_QueryDeviceList_Handler,
+		},
+		{
+			MethodName: "QueryDeviceById",
+			Handler:    _RpcDevice_QueryDeviceById_Handler,
 		},
 		{
 			MethodName: "CreateDevice",
