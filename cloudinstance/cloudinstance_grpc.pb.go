1defc6619eaa0b91c7618dbdff0dbcaa93626646
gongcheng <gongcheng.zhu@tuya.com>
2023-01-29 10:11:42 +0800

    fix

--- cloudinstance/cloudinstance_grpc.pb.go
+++ cloudinstance/cloudinstance_grpc.pb.go
@@ -24,6 +24,8 @@ const _ = grpc.SupportPackageIsVersion7
 type CloudInstanceServiceClient interface {
 	// 查询云服务示例信息 edge = s driver = c
 	QueryCloudInstanceByPlatform(ctx context.Context, in *QueryCloudInstanceByPlatformRequest, opts ...grpc.CallOption) (*QueryCloudInstanceByPlatformResponse, error)
+	// 上报驱动使用云实例
+	DriverReportPlatformInfo(ctx context.Context, in *DriverReportPlatformInfoRequest, opts ...grpc.CallOption) (*DriverReportPlatformInfoResponse, error)
 }
 
 type cloudInstanceServiceClient struct {
@@ -43,12 +45,23 @@ func (c *cloudInstanceServiceClient) QueryCloudInstanceByPlatform(ctx context.Co
 	return out, nil
 }
 
+func (c *cloudInstanceServiceClient) DriverReportPlatformInfo(ctx context.Context, in *DriverReportPlatformInfoRequest, opts ...grpc.CallOption) (*DriverReportPlatformInfoResponse, error) {
+	out := new(DriverReportPlatformInfoResponse)
+	err := c.cc.Invoke(ctx, "/cloudinstance.CloudInstanceService/DriverReportPlatformInfo", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CloudInstanceServiceServer is the server API for CloudInstanceService service.
 // All implementations must embed UnimplementedCloudInstanceServiceServer
 // for forward compatibility
 type CloudInstanceServiceServer interface {
 	// 查询云服务示例信息 edge = s driver = c
 	QueryCloudInstanceByPlatform(context.Context, *QueryCloudInstanceByPlatformRequest) (*QueryCloudInstanceByPlatformResponse, error)
+	// 上报驱动使用云实例
+	DriverReportPlatformInfo(context.Context, *DriverReportPlatformInfoRequest) (*DriverReportPlatformInfoResponse, error)
 	mustEmbedUnimplementedCloudInstanceServiceServer()
 }
 
@@ -58,6 +71,9 @@ type UnimplementedCloudInstanceServiceServer struct {
 
 func (UnimplementedCloudInstanceServiceServer) QueryCloudInstanceByPlatform(context.Context, *QueryCloudInstanceByPlatformRequest) (*QueryCloudInstanceByPlatformResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method QueryCloudInstanceByPlatform not implemented")
+}
+func (UnimplementedCloudInstanceServiceServer) DriverReportPlatformInfo(context.Context, *DriverReportPlatformInfoRequest) (*DriverReportPlatformInfoResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DriverReportPlatformInfo not implemented")
 }
 func (UnimplementedCloudInstanceServiceServer) mustEmbedUnimplementedCloudInstanceServiceServer() {}
 
@@ -90,6 +106,24 @@ func _CloudInstanceService_QueryCloudInstanceByPlatform_Handler(srv interface{},
 	return interceptor(ctx, in, info, handler)
 }
 
+func _CloudInstanceService_DriverReportPlatformInfo_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DriverReportPlatformInfoRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CloudInstanceServiceServer).DriverReportPlatformInfo(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/cloudinstance.CloudInstanceService/DriverReportPlatformInfo",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CloudInstanceServiceServer).DriverReportPlatformInfo(ctx, req.(*DriverReportPlatformInfoRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // CloudInstanceService_ServiceDesc is the grpc.ServiceDesc for CloudInstanceService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -100,6 +134,10 @@ var CloudInstanceService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "QueryCloudInstanceByPlatform",
 			Handler:    _CloudInstanceService_QueryCloudInstanceByPlatform_Handler,
+		},
+		{
+			MethodName: "DriverReportPlatformInfo",
+			Handler:    _CloudInstanceService_DriverReportPlatformInfo_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
