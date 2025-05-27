adc1bd4485c07f656a19bfe3c4beeb16bc33226a
zhuhoujiu <zhuhoujiu@winc-link.com>
2025-05-27 11:13:42 +0800

    feat-2.7

--- thingmodel/thingmodel_grpc.pb.go
+++ thingmodel/thingmodel_grpc.pb.go
@@ -199,3 +199,93 @@ var ThingModelDownService_ServiceDesc = grpc.ServiceDesc{
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "thingmodel/thingmodel.proto",
 }
+
+const (
+	MessageRateService_MessageRate_FullMethodName = "/thingmodel.MessageRateService/MessageRate"
+)
+
+// MessageRateServiceClient is the client API for MessageRateService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type MessageRateServiceClient interface {
+	MessageRate(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*MessageRateResponse, error)
+}
+
+type messageRateServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewMessageRateServiceClient(cc grpc.ClientConnInterface) MessageRateServiceClient {
+	return &messageRateServiceClient{cc}
+}
+
+func (c *messageRateServiceClient) MessageRate(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*MessageRateResponse, error) {
+	out := new(MessageRateResponse)
+	err := c.cc.Invoke(ctx, MessageRateService_MessageRate_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// MessageRateServiceServer is the server API for MessageRateService service.
+// All implementations must embed UnimplementedMessageRateServiceServer
+// for forward compatibility
+type MessageRateServiceServer interface {
+	MessageRate(context.Context, *emptypb.Empty) (*MessageRateResponse, error)
+	mustEmbedUnimplementedMessageRateServiceServer()
+}
+
+// UnimplementedMessageRateServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedMessageRateServiceServer struct {
+}
+
+func (UnimplementedMessageRateServiceServer) MessageRate(context.Context, *emptypb.Empty) (*MessageRateResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method MessageRate not implemented")
+}
+func (UnimplementedMessageRateServiceServer) mustEmbedUnimplementedMessageRateServiceServer() {}
+
+// UnsafeMessageRateServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to MessageRateServiceServer will
+// result in compilation errors.
+type UnsafeMessageRateServiceServer interface {
+	mustEmbedUnimplementedMessageRateServiceServer()
+}
+
+func RegisterMessageRateServiceServer(s grpc.ServiceRegistrar, srv MessageRateServiceServer) {
+	s.RegisterService(&MessageRateService_ServiceDesc, srv)
+}
+
+func _MessageRateService_MessageRate_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(emptypb.Empty)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MessageRateServiceServer).MessageRate(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: MessageRateService_MessageRate_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MessageRateServiceServer).MessageRate(ctx, req.(*emptypb.Empty))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// MessageRateService_ServiceDesc is the grpc.ServiceDesc for MessageRateService service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var MessageRateService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "thingmodel.MessageRateService",
+	HandlerType: (*MessageRateServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "MessageRate",
+			Handler:    _MessageRateService_MessageRate_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "thingmodel/thingmodel.proto",
+}
